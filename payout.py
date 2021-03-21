f31420b809bc9a4f21d353572c03a73721461227
Gareth Davies <hello@garethtdavies.com>
2021-03-21 04:05:17 +0000

    Working version of Foundation delegation

--- payout.py
+++ payout.py
@@ -10,6 +10,8 @@ from tabulate import tabulate
 import Currency
 import GraphQL
 import Mongo
+import os
+import math
 
 client = Mongo.Mongo()
 
@@ -17,13 +19,21 @@ client = Mongo.Mongo()
 # Define the payout calculation here
 ################################################################
 public_key = "B62qpge4uMq4Vv5Rvc8Gw9qSquUYd6xoW1pz7HQkMSHm6h1o7pvLPAN"  # Public key of the block producer
-ledger_hash = "jwkHcod9dcnhGfYx7t6yabSfckrKVwD6TJECs6oPSL8teYQE37Y"  # The ledger hash to use for calculations
+ledger_hash = "jx7buQVWFLsXTtzRgSxbYcT8EYLS8KCZbLrfDcJxMtyy4thw2Ee"  # The ledger hash to use for calculations
 staking_epoch = 0  # To ensure we only get blocks from the current staking epoch as the ledger may be different
 fee = 0.05  # The fee percentage to charge
-min_height = 1  # This can be the last known payout or this could vary the query to be a starting date
-confirmations = 0  # Can set this to any value for min confirmations up to `k`
+min_height = 0  # This can be the last known payout or this could vary the query to be a starting date
+confirmations = 12  # Can set this to any value for min confirmations up to `k`
+store = False  # Do we want to store this
+foundation_delegations = [
+    "B62qmsYXFNNE565yv7bEMPsPnpRCsMErf7J2v5jMnuKQ1jgwZS8BzXS",
+    "B62qn71s63yywMUCcFhP4iCata7HpgyrvmGjpKa1D9544vGW6FBZ6a1",
+    "B62qqEV4oP7w2jLQGckvZzdWjfdLKySKHJ3tNU5niRjpPD7beYumWTB",
+    "B62qmQAFPta1Q3c7wXHxXRKnE3uWyBYZCLb8frdHEgavi3BbBVkpeC1"
+]  # Could determine this from an API / predefined list but hardcoded for development
+coinbase = 720000000000  # Later we can set this dynamically - this is because we don't care about supercharged for Foundation
 
-# Get the latest block height from MinaExplorer
+# Get the latest block height
 latest_block = GraphQL.getLatestHeight()
 
 if not latest_block:
@@ -38,12 +48,14 @@ assert max_height <= latest_block["data"]["blocks"][0]["blockHeight"]
 
 print(f"This script will payout from blocks {min_height} to {max_height}")
 
-# Initialize variables
+# Initialize some stuff
 total_staking_balance = 0
+total_staking_balance_foundation = 0
 payouts = []
 all_blocks_total_rewards = 0
 all_blocks_total_fees = 0
 blocks_included = []
+store_payout = []
 
 # Get the staking ledger for an epoch
 try:
@@ -59,34 +71,42 @@ if not staking_ledger["data"]["stakes"]:
     exit("We have no stakers")
 
 for s in staking_ledger["data"]["stakes"]:
-    
+
     # Clean up timed weighting if no timing info
-    if not s["timing"] or not s["timing"]["timed_weighting"]:
+    if not s["timing"]:
         timed_weighting = 1
     else:
         timed_weighting = s["timing"]["timed_weighting"]
-    
-    # skip if delegated balance == 0
-    if float(s["balance"]) == 0:
-        print(f'Skipping {s["public_key"]} since balance is {s["balance"]}')
-        continue
-    
+
+    # Is this a Foundation address
+    if s["public_key"] in foundation_delegations:
+        foundation_delegation = True
+        total_staking_balance_foundation += s["balance"]
+    else:
+        foundation_delegation = False
+
     payouts.append({
-        "publicKey":
-        s["public_key"],
-        "total":
-        0,
-        "staking_balance":
-        s["balance"],
-        "timed_weighting": timed_weighting
+        "publicKey": s["public_key"],
+        "total": 0,
+        "staking_balance": s["balance"],
+        "timed_weighting": timed_weighting,
+        "foundation_delegation": foundation_delegation
     })
+
+    # Sum the total of the pool
     total_staking_balance += s["balance"]
 
 # DEBUG
 # print(payouts)
 
+assert (total_staking_balance_foundation <= total_staking_balance)
+
 # We now know the total pool staking balance with total_staking_balance
 print(f"The pool total staking balance is: {total_staking_balance}")
+
+print(
+    f"The Foundation delegation balance is: {total_staking_balance_foundation}"
+)
 
 # Who are we going to pay?
 print(f"There are {len(payouts)} delegates in the pool")
@@ -113,95 +133,199 @@ if not blocks["data"]["blocks"]:
 ################################################################
 for b in blocks["data"]["blocks"]:
 
+    # Keep track of payouts per block
+    foundation_payouts = 0
+    other_payouts = 0
+
+    # This will always be defined except when it is not...
+    if not b["transactions"]["coinbaseReceiverAccount"]:
+        print(
+            f"{b['blockHeight']} didn't have a coinbase so won it but no rewards."
+        )
+        break
+
+    coinbase_receiver = b["transactions"]["coinbaseReceiverAccount"][
+        "publicKey"]
+
     # Keep a log of all blocks we processed
     blocks_included.append(b["blockHeight"])
 
-    # This will always be defined even if it pays to the creator
-    coinbase_receiver = b["transactions"]["coinbaseReceiverAccount"]["publicKey"]
-
+    # This is to keep track of non-Foundation delegates
     sum_effective_pool_stakes = 0
     effective_pool_stakes = {}
 
-    # Determine the supercharged weighting for the block
-    supercharged_weighting = 1 + (
-        1 / (1 + int(b["txFees"]) / int(b["transactions"]["coinbase"])))
+    ####################################
+    # FEE TRANSFERS
+    ####################################
+    fee_transfers = list(
+        filter(lambda d: d['type'] in ["Fee_transfer"],
+               b["transactions"]["feeTransfer"]))
+    fee_transfers_by_coinbase = list(
+        filter(lambda d: d['type'] in ["Fee_transfer_via_coinbase"],
+               b["transactions"]["feeTransfer"]))
 
-    # What are the rewards for the block
-    total_rewards = int(b["transactions"]["coinbase"]) + int(
+    total_fee_transfers = sum(int(item['fee']) for item in fee_transfers)
+    # Note there can be more than 1 coinbase
+    fee_transfer_for_coinbase = sum(
+        int(item['fee']) for item in fee_transfers_by_coinbase)
+
+    # Sum all the fee transfers to this account with type of fee_transfer - these are the tx fees
+    fee_transfer_to_creator = list(
+        filter(lambda d: d['recipient'] in coinbase_receiver, fee_transfers))
+    total_fee_transfers_to_creator = sum(
+        int(item['fee']) for item in fee_transfer_to_creator)
+
+    # Sum all the fee transfers not to this account with type of fee_transfer - this is snark work for the included tx
+    fee_transfer_to_snarkers = total_fee_transfers - total_fee_transfers_to_creator
+
+    # Determine the supercharged weighting for the block
+
+    # New way uses fee transfers so we share the resulting profitability of the tx and take into account the coinbase snark
+    supercharged_weighting = 1 + (
+        1 / (1 + int(total_fee_transfers_to_creator) -
+             int(fee_transfer_to_snarkers)) /
+        (int(b["transactions"]["coinbase"]) - int(fee_transfer_for_coinbase)))
+
+    # What are the rewards for the block - this is how we used to calculate it
+    # this serves as a sense check currently to check logic
+    total_rewards_prev_method = int(b["transactions"]["coinbase"]) + int(
         b["txFees"]) - int(b["snarkFees"])
+
+    # Can also define this via fee transfers
+    total_rewards = int(
+        b["transactions"]["coinbase"]
+    ) + total_fee_transfers_to_creator - fee_transfer_to_snarkers - fee_transfer_for_coinbase
+
+    # We calculate rewards multiple ways to sense check
+    assert (total_rewards == total_rewards_prev_method)
 
     total_fees = int(fee * total_rewards)
 
     all_blocks_total_rewards += total_rewards
     all_blocks_total_fees += total_fees
 
-    # Let's run some checks to ensure we don't pay out more than we got, or find bugs ;)
-    if "feeTransfer" not in b["transactions"]:
-        # Just coinbase
-        assert total_rewards == int(b["transactions"]["coinbase"])
-    else:
-        # There were some fee transfers so let's _really_ make sure we don't pay out more than we received
-        fee_transfers = b["transactions"]["feeTransfer"]
-        bp_fee_transfers = [d for d in fee_transfers if d['recipient'] == coinbase_receiver]
-        sum_bp_fee_transfers = sum(int(item['fee']) for item in bp_fee_transfers)
+    #######################################################
+    # Determine the amount we need to pay the Foundation
+    # This algorithm is according to the published rules
+    # We don't need to account for supercharged rewards or
+    # share the transaction fees, so this is **good** for the pool
+    # as we share all these rewards. We first work out the Foundation
+    # payments and then subtract from the total rewards before sharing
+    # the remainder among the pool
 
-        # So assert that the value we are paying out is equal to what we received
-        total_rewards_fee_transfer = int(b["transactions"]["coinbase"]) + sum_bp_fee_transfers
-        # There is an issue here with the type `fee_transfer_via_coinbase` as we actually need to subtract
-        # this from the amount to get the amount we received via fee transfers. We can't get this via GraphQL
-        # so the best we can do is assert we are playing equal or less than this amount. This will be updated when
-        # the fix is landed.
-        assert(total_rewards <= total_rewards_fee_transfer)
-
-    # Loop through our list of delegates to determine the weighting per block
     for p in payouts:
-        supercharged_contribution = (
-            (supercharged_weighting - 1) * p["timed_weighting"]) + 1
-        effective_stake = p["staking_balance"] * supercharged_contribution
-        effective_pool_stakes[p["publicKey"]] = effective_stake
-        sum_effective_pool_stakes += effective_stake
 
-    # Sense check the effective pool stakes must be at least equal to total_staking_balance and less than 2x
-    assert total_staking_balance <= sum_effective_pool_stakes <= 2 * total_staking_balance
+        if p["foundation_delegation"]:
+
+            # Only pay foundation a % of the normal coinbase
+            # Round down to the nearest nanomina
+            foundation_block_total = math.floor(
+                (p["staking_balance"] / total_staking_balance) * coinbase *
+                (1 - fee))
+
+            p["total"] += foundation_block_total
+
+            store_payout.append({
+                "publicKey": p["publicKey"],
+                "blockHeight": b["blockHeight"],
+                "stateHash": b["stateHash"],
+                "totalPoolStakes": total_staking_balance,
+                "stakingBalance": p["staking_balance"],
+                "dateTime": b["dateTime"],
+                "coinbase": int(b["transactions"]["coinbase"]),
+                "totalRewards": total_rewards,
+                "payout": foundation_block_total,
+                "epoch": staking_epoch,
+                "ledgerHash": ledger_hash,
+                "foundation": True
+            })
+
+            # Track all the Foundation payouts
+            foundation_payouts += foundation_block_total
+        else:
+            # This was a non foundation address
+            # So calculate this the other way
+            supercharged_contribution = (
+                (supercharged_weighting - 1) * p["timed_weighting"]) + 1
+            effective_stake = p["staking_balance"] * supercharged_contribution
+            # This the effective percentage of the pool disregarding the Foundation element
+            effective_pool_stakes[p["publicKey"]] = effective_stake
+            sum_effective_pool_stakes += effective_stake
+
+    # Check here the balances make sense
+    assert (foundation_payouts <= total_rewards)
+
+    assert (sum_effective_pool_stakes <= 2 * total_staking_balance)
+
+    # What are the remaining rewards we can share? This should always be higher than if we don't share.
+    block_pool_share = total_rewards - (foundation_payouts / (1 - fee))
 
     # Determine the effective pool weighting based on sum of effective stakes
     for p in payouts:
-        effective_pool_weighting = effective_pool_stakes[
-            p["publicKey"]] / sum_effective_pool_stakes
+        if not p["foundation_delegation"]:
+            effective_pool_weighting = effective_pool_stakes[
+                p["publicKey"]] / sum_effective_pool_stakes
 
-        #This must be less than 1 or we have a major issue
-        assert effective_pool_weighting <= 1
+            #This must be less than 1 or we have a major issue
+            assert effective_pool_weighting <= 1
 
-        block_total = round(
-            (total_rewards - total_fees) * effective_pool_weighting)
-        p["total"] += block_total
+            block_total = math.floor(block_pool_share *
+                                     effective_pool_weighting * (1 - fee))
+            p["total"] += block_total
 
-        # Store this data in a structured format for later querying and for the payment script, handled seperately
-        store_payout = {
-            "publicKey": p["publicKey"],
-            "blockHeight": b["blockHeight"],
-            "stateHash": b["stateHash"],
-            "totalPoolStakes": total_staking_balance,
-            "effectivePoolWeighting": effective_pool_weighting,
-            "effectivePoolStakes": effective_pool_stakes[p["publicKey"]],
-            "stakingBalance": p["staking_balance"],
-            "sumEffectivePoolStakes": sum_effective_pool_stakes,
-            "superChargedWeighting": supercharged_weighting,
-            "dateTime": b["dateTime"],
-            "coinbase": int(b["transactions"]["coinbase"]),
-            "totalRewards": total_rewards,
-            "payout": block_total,
-            "epoch": staking_epoch,
-            "ledgerHash": ledger_hash
-        }
+            other_payouts += block_total
 
-        try:
-            #post_id = client.collection.insert_one(store_payout)
-            # Store this somewhere I am using Mongo
-            pass
-        except Exception as e:
-            print(e)
-            exit("There was an issue storing a payout")
+            # Store this data in a structured format for later querying and for the payment script, handled seperately
+            store_payout.append({
+                "publicKey":
+                p["publicKey"],
+                "blockHeight":
+                b["blockHeight"],
+                "stateHash":
+                b["stateHash"],
+                "totalPoolStakes":
+                total_staking_balance,
+                "effectivePoolWeighting":
+                effective_pool_weighting,
+                "effectivePoolStakes":
+                effective_pool_stakes[p["publicKey"]],
+                "superchargedContribution":
+                supercharged_contribution,
+                "stakingBalance":
+                p["staking_balance"],
+                "sumEffectivePoolStakes":
+                sum_effective_pool_stakes,
+                "superChargedWeighting":
+                supercharged_weighting,
+                "dateTime":
+                b["dateTime"],
+                "coinbase":
+                int(b["transactions"]["coinbase"]),
+                "totalRewards":
+                total_rewards,
+                "payout":
+                block_total,
+                "epoch":
+                staking_epoch,
+                "ledgerHash":
+                ledger_hash
+            })
+
+    # Final check
+    # These are essentially the same but we allow for a tiny bit of nanomina rounding and worst case we never pay more
+    assert (foundation_payouts + other_payouts + total_fees <= total_rewards)
+
+# Store the payouts here so we can generate transactions
+if store:
+
+    if not os.getenv('MONGO_URI'):
+        exit("No Mongo connection string provided")
+
+    try:
+        post_id = client.collection.insert_many(store_payout)
+    except Exception as e:
+        print(e)
+        exit("There was an issue storing a payout")
 
 ################################################################
 # Print some helpful data to the screen
@@ -216,7 +340,7 @@ print("That is " +
                         format=Currency.CurrencyFormat.NANO).decimal_format() +
       " mina")
 
-print(f"Our fee is " +
+print("Our fee is " +
       Currency.Currency(all_blocks_total_fees,
                         format=Currency.CurrencyFormat.NANO).decimal_format() +
       " mina")
@@ -226,16 +350,23 @@ payout_json = []
 
 for p in payouts:
     payout_table.append([
-        p["publicKey"], p["total"],
+        p["publicKey"],
         Currency.Currency(
-            p["total"], format=Currency.CurrencyFormat.NANO).decimal_format()
+            p["staking_balance"],
+            format=Currency.CurrencyFormat.WHOLE).decimal_format(), p["total"],
+        Currency.Currency(
+            p["total"], format=Currency.CurrencyFormat.NANO).decimal_format(),
+        p["foundation_delegation"]
     ])
 
     payout_json.append({"publicKey": p["publicKey"], "total": p["total"]})
 
 print(
     tabulate(payout_table,
-             headers=["PublicKey", "Payout nanomina", "Payout mina"],
+             headers=[
+                 "PublicKey", "Staking Balance", "Payout nanomina",
+                 "Payout mina", "Foundation"
+             ],
              tablefmt="pretty"))
 
 # This is the payout json we are taking the the next stage to sign. So store this somewhere.
